a38c64b106dd2eb5ca158827c62394cf79bf9e05
unknown <katiamercado97@gmail.com>
2026-01-12 17:35:32 -0600

    Primer Componente

---
+++ src/FirstStepsApp.tsx
@@ -0,0 +1,15 @@
+export function FirstStepsApp() {
+
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Esto es un parrafo</p>
+            <button>Click aquí</button>
+
+            <div>
+               <h2>Hola dentro del div</h2>
+            </div>
+        </>
+    );
+}
---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,14 @@
+export function MyAwesomeApp() {
+    return(
+        <>
+            <h1>Katt</h1>
+            <h3>Mercado</h3>
+        </>
+    );
+}
+
+/* ES IGUAL A ESTO v
+    export const MyAwesomeApp = () => {
+        blablabla
+    }
+*/
